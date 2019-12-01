ebf491576a74c0ea1bca3f7377d41150902138e8
Petch <kittichan@wongnai.com>
2019-12-01 09:24:38 +0700

    Add statefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -7,36 +7,96 @@ class QuoteApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Quote',
-      home: HomePage(),
+      home: StatefulHomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class Quote {
+  final String text;
+  final String author;
+  Quote(this.text, this.author);
+}
+
+class StatefulHomePage extends StatefulWidget {
+  @override
+  _StatefulHomePageState createState() => _StatefulHomePageState();
+}
+
+class _StatefulHomePageState extends State<StatefulHomePage> {
+  final _formkey = GlobalKey<FormState>();
+
+  String _inputQuote;
+  String _inputAuthor;
+
+  List<Quote> quotes = [] ;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Quote'),
-      ),
-      body: ListView(
-        children: <Widget>[
-          QuoteCard("1111","2222","https://sportshub.cbsistatic.com/i/r/2019/11/20/d02262c9-3d1c-4c47-9f7f-02cfea79f607/thumbnail/1200x675/c1aeae444c56e9c97e71796535ca83db/lebron.jpg"),
-          QuoteCard("1111","2222","https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/1966.png"),
-          QuoteCard("1111","2222","https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpeopledotcom.files.wordpress.com%2F2019%2F10%2Flebron-james.jpg&w=400&c=sc&poi=face&q=85"),
-          QuoteCard("1111","2222","https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpeopledotcom.files.wordpress.com%2F2019%2F10%2Flebron-james.jpg&w=400&c=sc&poi=face&q=85"),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Quote'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Form(
+              key: _formkey,
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Quote'
+                    ),
+                    onSaved: (String value){
+                      _inputQuote = value;
+                    },
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Author'
+                    ),
+                    onSaved: (String value){
+                      _inputAuthor = value;
+                    },
+                  ),
+                  RaisedButton(
+                    onPressed: (){
+                      _formkey.currentState.save();
+                      setState(() {
+                        quotes.insert(0, Quote(_inputQuote, _inputAuthor));
+                      });
+                      
+                       _formkey.currentState.reset();
+                    },
+                    child: Text('Add'),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: quotes.length == 0
+                              ? Center(
+                      child: Text('Empty'),
+                    )
+                  : ListView.builder(
+                      itemBuilder: (BuildContext context, int index) {
+                        return QuoteCard(quotes[index].text, quotes[index].author);
+                      },
+                      itemCount: quotes.length,
+                    ),
+            ),
+          ],
+        ));
   }
 }
 
 class QuoteCard extends StatelessWidget {
   final String _text;
   final String _author;
-  final String _imgUrl;
 
-  const QuoteCard(this._text,this._author,this._imgUrl,{
+  const QuoteCard(
+    this._text,
+    this._author, {
     Key key,
   }) : super(key: key);
 
@@ -47,15 +107,6 @@ class QuoteCard extends StatelessWidget {
       elevation: 10,
       child: Column(
         children: <Widget>[
-          Container(
-            height: 300,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(4),
-                image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage(
-                        '$_imgUrl'))),
-          ),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Text(
