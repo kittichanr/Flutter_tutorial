e7dcc09f265b45166964d275d0ef331dcc0eef06
Petch <petchkubb@live.com>
2019-12-05 07:42:05 +0700

    get json data from server to view

--- lib/pages/vga-detail.dart
+++ lib/pages/vga-detail.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial_1/models/vga.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class VgaDetailPage extends StatelessWidget {
+  final Vga vga;
+  VgaDetailPage(this.vga);
   @override
   Widget build(BuildContext context) {
+    var v = vga;
     return Scaffold(
       appBar: AppBar(
         title: Text('DetailPage'),
       ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Back'),
-          onPressed: () => Navigator.pop(context),
-        ),
-      ),
+      body: Container(
+          child: Column(
+        children: <Widget>[
+          Center(
+            child: Container(
+              height: 300,
+              width: 300,
+              child: CachedNetworkImage(
+                imageUrl: 'https://www.advice.co.th/pic-pc/vga/${v.vgaPicture}',
+              ),
+            ),
+          ),
+          RaisedButton(
+            child: Text('Back'),
+            onPressed: () => Navigator.pop(context),
+          ),
+        ],
+      )),
     );
   }
 }
--- lib/pages/vga.dart
+++ lib/pages/vga.dart
@@ -1,8 +1,43 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'vga-detail.dart';
+import 'package:flutter_cache_store/flutter_cache_store.dart';
 
-class VgaPage extends StatelessWidget {
+import 'package:flutter_tutorial_1/models/vga.dart';
+
+class VgaPage extends StatefulWidget {
+  @override
+  _VgaPageState createState() => _VgaPageState();
+}
+
+class _VgaPageState extends State<VgaPage> {
+  List<Vga> vgas = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final store = await CacheStore.getInstance();
+    File file = await store.getFile('https://www.advice.co.th/pc/get_comp/vga');
+    final jsonString = json.decode(file.readAsStringSync());
+
+    setState(() {
+      jsonString.forEach((v) {
+        final vga = Vga.fromJson(v);
+        if (vga.advId != '') {
+          vgas.add(vga);
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,29 +46,44 @@ class VgaPage extends StatelessWidget {
       ),
       body: Center(
         child: ListView.builder(
-          itemCount: 3,
+          itemCount: vgas.length,
           itemBuilder: (context, i) {
-            return GestureDetector(
-                onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => VgaDetailPage(),
-                    )),
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      height: 150,
-                      width: 150,
-                      child: CachedNetworkImage(
-                        imageUrl:
-                            "https://static01.nyt.com/images/2018/05/03/us/03spongebob_xp/03spongebob_xp-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
-                        placeholder: (context, url) =>
-                            CircularProgressIndicator(),
-                        errorWidget: (context, url, error) => Icon(Icons.error),
-                      ),
+            var v = vgas[vgas.length - i - 1];
+            return Card(
+                elevation: 0,
+                child: Container(
+                  child: InkWell(
+                    onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => VgaDetailPage(v),
+                        )),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          height: 100,
+                          width: 100,
+                          child: CachedNetworkImage(
+                            imageUrl:
+                                'https://www.advice.co.th/pic-pc/vga/${v.vgaPicture}',
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            padding: EdgeInsets.all(8),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text('${v.vgaBrand} ${v.vgaModel}'),
+                                Text('${v.vgaPriceAdv}'),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                    Text('$i')
-                  ],
+                  ),
                 ));
           },
         ),
